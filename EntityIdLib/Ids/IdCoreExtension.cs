fa414b770cfc9909adefa3465110bf1cee3df0bc
DM <dm@mdsrv.net>
2019-10-01 11:56:31 +0000

    Update IdCoreExtension.cs

--- EntityIdLib/Ids/IdCoreExtension.cs
+++ EntityIdLib/Ids/IdCoreExtension.cs
@@ -5,7 +5,7 @@ namespace EntityIdLib.Ids
 {
     public static class IdCoreExtension
     {
-        public static IdConverter<T> GetConverter<T, TC>(this IIdBase<T, TC> obj)
+        public static IdConverter<T> GetConverter<T, TC>(this TC obj)
             where TC : IIdBase<T, TC>
         {
             return IdCore.Instance.GetConverter<T, TC>();
