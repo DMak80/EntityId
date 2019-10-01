36ded4849249c9fe4138fe9213d74e6d1e06bf05
Denis Makaev <dm@mdsrv.net>
2019-10-01 22:08:31 +0300

    Update IdCoreExtension.cs. fix ConverterExtensions

--- EntityIdLib/Converters/ConverterExtensions.cs
+++ EntityIdLib/Converters/ConverterExtensions.cs
@@ -6,16 +6,16 @@ namespace EntityIdLib.Converters
 {
     public static class ConverterExtensions
     {
-        public static Uid ToUid<T, TC>(this IIdBase<T, TC> obj)
+        public static Uid ToUid<T, TC>(this TC obj)
             where TC : IIdBase<T, TC>
         {
-            return obj.GetConverter().ToUid(obj.Id);
+            return obj.GetConverter<T, TC>().ToUid(obj.Id);
         }
 
         public static TC ToId<T, TC>(this Uid obj)
             where TC : IIdBase<T, TC>
         {
-            return (TC) Activator.CreateInstance(typeof(TC), default(TC).GetConverter().FromUid(obj));
+            return (TC) Activator.CreateInstance(typeof(TC), default(TC).GetConverter<T, TC>().FromUid(obj));
         }
     }
 }
